3904e3c8acaa41f935bf6c674569f5a9cb1333fc
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-11-12 13:29:20 +0000

    [add]投稿フォーム機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:new, :index, :show]
+  resources :users, onry: [:show, :edit]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
