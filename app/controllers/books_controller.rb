ddd692cb4560d0799f8a4c6e0572b2590fb89927
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-11-14 13:32:19 +0000

    [add]フラッシュメッセージ実装、bookedit機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,16 +7,35 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
+    flash[:notice]="You have created book successfully."
     redirect_to books_path
   end
 
   def index
     @books = Book.all
+    @user = current_user
   end
 
   def show
     @books = Book.find(params[:id])
     @user = @books.user
+  end
+
+  def edit
+    @books = Book.find(params[:id])
+  end
+
+  def update
+    @books = Book.find(params[:id])
+    @books.update(book_params)
+    flash[:notice]="You have updated book successfully."
+    redirect_to book_path(@books.id)
+  end
+
+  def destroy
+    @books = Book.find(params[:id])
+    @books.destroy
+    redirect_to book_path
   end
 
   private
